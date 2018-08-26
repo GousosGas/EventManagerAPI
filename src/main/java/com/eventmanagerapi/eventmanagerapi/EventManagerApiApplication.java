17da381784a676b28ba5091dece6d3a1500ba9f8
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-08-26 18:27:19 +0300

    first commit

---
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/EventManagerApiApplication.java
@@ -0,0 +1,12 @@
+package com.eventmanagerapi.eventmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eventmanagerapi/eventmanagerapi/EventManagerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eventmanagerapi.eventmanagerapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventManagerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
