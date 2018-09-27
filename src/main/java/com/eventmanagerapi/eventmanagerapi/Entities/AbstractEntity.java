c0893e53d92bc1f5be14eca5be1899fca3df39a8
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-27 18:26:08 +0300

    equals and hashCode implemented to Enitites

---
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/Entities/AbstractEntity.java
@@ -0,0 +1,34 @@
+package com.eventmanagerapi.eventmanagerapi.Entities;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@MappedSuperclass
+public class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    @Column(nullable = false,updatable = false)
+    protected Long id;
+    @CreationTimestamp
+    @Column(updatable = false)
+    protected Instant created;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+}
