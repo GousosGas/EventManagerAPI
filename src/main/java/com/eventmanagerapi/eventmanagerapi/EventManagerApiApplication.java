73f0543a1aabc396e3cd0ca97a12241d724374b5
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-27 19:03:05 +0300

    jsr310 imported,hibernate updated, Converter for timezone added

--- src/main/java/com/eventmanagerapi/eventmanagerapi/EventManagerApiApplication.java
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/EventManagerApiApplication.java
@@ -2,8 +2,11 @@ package com.eventmanagerapi.eventmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {EventManagerApiApplication.class, Jsr310Converters.class})
 public class EventManagerApiApplication {
 
 	public static void main(String[] args) {
