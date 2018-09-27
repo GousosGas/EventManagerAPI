70cf62dfa628965c263454bfc48429e52c5a419f
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-27 22:48:18 +0300

    find by name functionality developed

--- src/main/java/com/eventmanagerapi/eventmanagerapi/Repositories/EventRepository.java
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/Repositories/EventRepository.java
@@ -2,6 +2,10 @@ package com.eventmanagerapi.eventmanagerapi.Repositories;
 
 import com.eventmanagerapi.eventmanagerapi.Entities.Event;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EventRepository extends PagingAndSortingRepository<Event,Long> {
+    List<Event> findByName(@Param("name") String name);
 }
