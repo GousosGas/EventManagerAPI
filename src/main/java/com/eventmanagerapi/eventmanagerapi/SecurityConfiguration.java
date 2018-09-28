b7c5721594065e1852a51e6d438cd13525719f71
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-28 17:31:17 +0300

    http authorization developed csrf disabled

--- src/main/java/com/eventmanagerapi/eventmanagerapi/SecurityConfiguration.java
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.eventmanagerapi.eventmanagerapi;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,7 +24,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/events")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/events/**")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH,"/events/**")
+                .hasRole("ADMIN")
+                .and().csrf().disable();
+
     }
 }
 
