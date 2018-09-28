60da66726805613eb2a6695ba4ec6c0e10aec8d9
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-28 16:00:20 +0300

    Exposed hiiden field created at of event

--- src/main/java/com/eventmanagerapi/eventmanagerapi/Entities/Projections/PartialEventProjection.java
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/Entities/Projections/PartialEventProjection.java
@@ -3,6 +3,7 @@ package com.eventmanagerapi.eventmanagerapi.Entities.Projections;
 import com.eventmanagerapi.eventmanagerapi.Entities.Event;
 import org.springframework.data.rest.core.config.Projection;
 
+import java.time.Instant;
 import java.time.ZonedDateTime;
 
 @Projection(name="partial",types = {Event.class})
@@ -11,4 +12,5 @@ public interface PartialEventProjection {
     String getName();
     ZonedDateTime getStartTime();
     ZonedDateTime getEndTime();
+    Instant getCreated();
 }
