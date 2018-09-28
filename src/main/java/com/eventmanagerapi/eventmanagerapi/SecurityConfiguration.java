1d2b46e801a985a23db36228cf809ad0e4b1e974
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-28 17:38:46 +0300

    Password encoder imported

--- src/main/java/com/eventmanagerapi/eventmanagerapi/SecurityConfiguration.java
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
@@ -12,13 +14,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication()
                 .withUser("root")
-                .password("root")
+                .password(encoder.encode("root"))
                 .roles("USER")
                 .and()
                 .withUser("admin")
-                .password("admin")
+                .password(encoder.encode("admin"))
                 .roles("ADMIN");
     }
 
