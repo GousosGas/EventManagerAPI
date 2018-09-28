fc85b01e08b5b0e886bf018ff85b06ef79b72278
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-28 16:56:17 +0300

    Hal Broswer imported

--- src/main/java/com/eventmanagerapi/eventmanagerapi/Repositories/EventRepository.java
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/Repositories/EventRepository.java
@@ -1,13 +1,16 @@
 package com.eventmanagerapi.eventmanagerapi.Repositories;
 
 import com.eventmanagerapi.eventmanagerapi.Entities.Event;
+import com.eventmanagerapi.eventmanagerapi.Entities.Projections.PartialEventProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.time.ZoneId;
 
+@RepositoryRestResource(excerptProjection = PartialEventProjection.class)
 public interface EventRepository extends PagingAndSortingRepository<Event,Long> {
     Page<Event> findByName(@Param("name") String name, Pageable pageable);
     Page<Event> findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
