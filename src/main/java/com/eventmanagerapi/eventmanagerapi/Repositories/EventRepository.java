60a2dc319649fc6302bf520dd89590f22e7303ae
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-09-28 14:19:46 +0300

    filter by zone id

--- src/main/java/com/eventmanagerapi/eventmanagerapi/Repositories/EventRepository.java
+++ src/main/java/com/eventmanagerapi/eventmanagerapi/Repositories/EventRepository.java
@@ -1,11 +1,14 @@
 package com.eventmanagerapi.eventmanagerapi.Repositories;
 
 import com.eventmanagerapi.eventmanagerapi.Entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import java.time.ZoneId;
 
 public interface EventRepository extends PagingAndSortingRepository<Event,Long> {
-    List<Event> findByName(@Param("name") String name);
+    Page<Event> findByName(@Param("name") String name, Pageable pageable);
+    Page<Event> findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
 }
